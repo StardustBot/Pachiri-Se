c51d6228247ee5f3f6306353ccce70317185155e
Nox <aden.lequire@gmail.com>
2017-10-05 22:36:46 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -20,35 +20,37 @@ switch (args[0].toLowerCase()) {
     case "ping":
         message.channel.send("Pong!");
     break;
-case "help":
-            message.channel.send({
-                embed: {
-                    color: EMBCOLOR,
-                    author: {
-                        "name": "Star Fragments",
-                        "url": "https://github.com/StardustBot/Stardust",
-                        "icon_url": "https://i.pinimg.com/736x/2f/6b/a6/2f6ba6c37cd47061ffb209797e50bc7a--anime-kawaii-anime-chibi.jpg"
-                    },
-                    fields: [
-                    {
-                        name: "Main",
-                        value: "help\info\ping"
-                    },        
-                    {
-                        name: "Roleplay",
-                        value: "bite\hug\pat\shrug\thumbsup\wag\weapon"
-                    
-                    },                    
-                    {
-                        name: "Music",
-                        value: "musichelp\wowaka\nayusenjin\daniwell\kuruosa\pinnochio\materu\mitchie\40mp\halysoy\mothy\circus\crusher\juby\rachie"
-                    }
-                    ]
-                }
-            });
-            break;
+	
+	case "help":
+			message.channel.send({
+				embed: {
+					color: EMBCOLOR,
+					author: {
+						"name": "Nano Commands",
+						"url": "https://github.com/heyimnox/nano-bot",
+						"icon_url": "http://i0.kym-cdn.com/profiles/icons/big/000/041/786/steame.jpg"
+					},
+					fields: [
+					{
+						name: "Main",
+						value: "help\ninfo\nping"
+					},		
+					{
+						name: "Roleplay",
+						value: "bite\nhug\npat\nshrug\nthumbsup\nwag\nweapon"
+					},					
+					{
+						name: "Music",
+						value: "musichelp\nwowaka\nnayusenjin\ndaniwell\nkuruosa\npinnochio\nmateru\nmitchie\n40mp\nhalysoy\nmothy\ncircus\ncrusher\njuby\nrachie"
+					}
+					]
+				}
+			});
+	break;
+	
+}
 
 });
 
-bot.login(process.env.BOT_TOKEN);
-
+bot.login("MzY1NjU5Mzk2MjQ1NTUzMTU0.DLhrVg.w_3WhfgLCFbWYaTqlWAqa3Lzn8c");
+//bot.login(process.env.BOT_TOKEN);
