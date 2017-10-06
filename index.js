3b660ad3a10bc1835e164726262d15a55e0907d3
StarDust! <32558399+StardustBot@users.noreply.github.com>
2017-10-05 22:21:32 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -20,8 +20,35 @@ switch (args[0].toLowerCase()) {
     case "ping":
         message.channel.send("Pong!");
     break;
-}
+}case "help":
+            message.channel.send({
+                embed: {
+                    color: EMBCOLOR,
+                    author: {
+                        "name": "Star Fragments",
+                        "url": "https://github.com/StardustBot/Stardust",
+                        "icon_url": "https://i.pinimg.com/736x/2f/6b/a6/2f6ba6c37cd47061ffb209797e50bc7a--anime-kawaii-anime-chibi.jpg"
+                    },
+                    fields: [
+                    {
+                        name: "Main",
+                        value: "help\info\ping"
+                    },        
+                    {
+                        name: "Roleplay",
+                        value: "bite\hug\pat\shrug\thumbsup\wag\weapon"
+                    
+                    },                    
+                    {
+                        name: "Music",
+                        value: "musichelp\wowaka\nayusenjin\daniwell\kuruosa\pinnochio\materu\mitchie\40mp\halysoy\mothy\circus\crusher\juby\rachie"
+                    }
+                    ]
+                }
+            });
+            break;
 
 });
 
 bot.login(process.env.BOT_TOKEN);
+
