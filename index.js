6b6b25bf12e8baa20586df11350d3c08a37e6ad9
StarDust! <32558399+StardustBot@users.noreply.github.com>
2017-10-06 20:24:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ switch (args[0].toLowerCase()) {
 					},					
 					{
 						name: "Music",
-						value: "musichelp\nwowaka\nnayusenjin\ndaniwell\nkuruosa\npinnochio\nmateru\nmitchie\n40mp\nhalysoy\nmothy\ncircus\ncrusher\njuby\nrachie"
+						value: "musichelp\nwowaka\nnayutanseijin\ndaniwell\nkuruosa\npinnochio\nmateru\nmitchiem\n40mp\nhalysoy\nmothy\ncircus\ncrusher\njuby\nrachie"
 					}
 					]
 				}
@@ -74,10 +74,89 @@ switch (args[0].toLowerCase()) {
 				}
 			});
 	break;
-					
+	
+	case "nayutanseijin":
+		 	message.channel.send([
+				embed: {
+					color: EMBCOLOR,
+					author: {
+						"name": "Nayutan Seijin (NayutAlien) profile",
+						"icon_url":"https://t00.deviantart.net/_gJ6XZ8myhLxCxRZdshp9jel5AE=/fit-in/700x350/filters:fixed_height(100,100):origin()/pre00/809b/th/pre/f/2017/074/7/3/alien_alien_by_kurohaai-db2edth.png",
+						"url": "http://www.nicovideo.jp/user/49716308"
+					},
+					fields: [
+					{
+						name: "Recent",
+						value: "Reverse Universe\nhttp://www.nicovideo.jp/watch/sm31843582"
+					},		
+					{
+						name: "All Music",
+						value: "http://www.nicovideo.jp/mylist/50857506"
+					},					
+					{
+						name: "Status",
+						value: "Active as of August 2017"
+					}
+					]
+				}
+			});
+	break;
+	
+	case "mitchie":
+		 	message.channel.send([
+				embed: {
+					color: EMBCOLOR,
+					author: {
+						"name": "MITCHIE M profile",
+						"icon_url":"https://vignette.wikia.nocookie.net/vocaloid/images/8/89/MitchieM.jpg/revision/latest/scale-to-width-down/100?cb=20120613065723",						
+						"url": "http://www.nicovideo.jp/user/22580750a"
+					},
+					fields: [
+					{
+						name: "Recent",
+						value: "Ohedo Juliet Night\nhttp://www.nicovideo.jp/watch/sm31812792"
+					},		
+					{
+						name: "All Music",
+						value: "http://www.nicovideo.jp/mylist/26375614"
+					},					
+					{
+						name: "Status",
+						value: "Active as of September 2017"
+					}
+					]
+				}
+			});
+	break;	
 		
-				
+	case "40mp":
+		 	message.channel.send([
+				embed: {
+					color: EMBCOLOR,
+					author: {
+						"name": "40MeterP profile",
+						"icon_url":"https://vignette.wikia.nocookie.net/vocaloid/images/8/89/MitchieM.jpg/revision/latest/scale-to-width-down/100?cb=20120613065723",						
+						"url": "http://www.nicovideo.jp/user/49716308"
+					},
+					fields: [
+					{
+						name: "Recent",
+						value: "Ohedo Juliet Night\nhttp://www.nicovideo.jp/watch/sm31812792"
+					},		
+					{
+						name: "All Music",
+						value: "http://www.nicovideo.jp/mylist/26375614"
+					},					
+					{
+						name: "Status",
+						value: "Active as of September 2017"
+					}
+					]
+				}
+			});
+	break;	
 }
+	
 
 });
 
